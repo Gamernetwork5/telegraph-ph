57d5c05b4772884506844fd76bba34193995787b
Gamernetwork5 <153068621+Gamernetwork5@users.noreply.github.com>
2023-12-26 08:52:43 +0530

    Add files via upload

---
+++ helper/txt.py
@@ -0,0 +1,34 @@
+class Txt(object):
+
+    START_TXT = """<b>Hey {} 👋
+I am {}
+Send any photo/video below 5mb to get telegraph link
+𝚃𝚑𝚒𝚜 𝙱𝚘𝚝 𝚒𝚜 𝙳𝚎𝚟𝚎𝚕𝚘𝚙𝚎𝚍 𝙱𝚢 <a href=https://t.me/TGBotsCode>𝙏𝙃𝙀 𝙋𝙎 𝘽𝙊𝙏𝙎</a>💞</b>"""
+
+    ABOUT_TXT = """
+╭───────────⍟
+├🤖 𝙼𝚈 𝙽𝙰𝙼𝙴 : {}
+├👑 𝙳𝙴𝚅𝙴𝙻𝙾𝙿𝙴𝚁𝚂 : <a href=https://t.me/TGBotsCode>𝙏𝙃𝙀 𝙋𝙎 𝘽𝙊𝙏𝙎</a> 
+├👨‍💻 𝙿𝚁𝙾𝙶𝚁𝙰𝙼𝙴𝚁 : <a href=https://github.com/TGBotsCode>𝙏𝙃𝙀 𝙋𝙎 𝘽𝙊𝙏𝙎</a>
+├📕 𝙻𝙸𝙱𝚁𝙰𝚁𝚈 : <a href=https://github.com/pyrogram>𝙿𝚈𝚁𝙾𝙶𝚁𝙰𝙼</a>
+├✏️ 𝙻𝙰𝙽𝙶𝚄𝙰𝙶𝙴 : <a href=https://www.python.org>𝙿𝚈𝚃𝙷𝙾𝙽 3</a>
+├💾 𝙳𝙰𝚃𝙰 𝙱𝙰𝚂𝙴 : <a href=https://cloud.mongodb.com>𝙼𝙾𝙽𝙶𝙾𝙳𝙱</a>
+├📊 𝙱𝚄𝙸𝙻𝙳 𝚂𝚃𝙰𝚄𝚂 : v2.1.90              
+╰───────────────⍟
+                                """
+    HELP_TXT = """
+🌌 <b><u>HOW TO USE THIS BOT</u></b>
+  
+•> /start a bot and send any picture to automatically upload to bot.
+•> /this bot support media type only.
+•> /supporting media type mp4,img,jpg,gif etc.
+•> / only support 5 mb files
+"""
+
+#⚠️ don't remove our credits 🙏😢😢
+    DEV_TXT = """
+<b><u>Source Code </b></u> 
+
+» 𝗦𝗢𝗨𝗥𝗖𝗘 𝗖𝗢𝗗𝗘 : <a href=https://github.com/DX-MODS/Telegraph-Bot</a>
+» 𝗛𝗢𝗪 𝗧𝗢 𝗗𝗘𝗣𝗟𝗢𝗬 : <a href=https://youtube.com/@DX-MODS</a>
+"""
